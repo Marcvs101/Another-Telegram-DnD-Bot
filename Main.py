55c6e3b7446b8127dbf967bf0b662330d8186e81
Marcvs101 <marcocuoci@yahoo.it>
2019-03-21 21:43:10 +0100

    Merge of roll

--- Main.py
+++ Main.py
@@ -126,29 +126,7 @@ def handler_messaggio(msg):
         elif comando.startswith("/roll"):
             print(str(mittente_username)+" invoked /roll")
             print(str(time.time())+" : "+str(mittente_username)+" invoked /roll",file=logfile)
-            parametri = comando.lower().strip().split(" ")
-            somma_dadi = 0
-            somma_costanti = 0
-            stringa = "@"+str(mittente_username)+"\n"
-            for token in parametri:
-                if (token.isdecimal()):
-                    somma_costanti += int(token)
-                else:
-                    dice = token.split("d")
-                    dice_type = int(dice[1])
-                    dice_number = 1
-                    if (dice[0] != ""):
-                        dice_number = int(dice[0])
-                    stringa += "Tirando "+str(dice_number)+" volte un D"+str(dice_type)+" ho ottenuto:\n"
-                    for _ in range(dice_number):
-                        dice_roll = randint(1, dice_type)
-                        stringa += "- "+str(dice_roll)+"\n"
-                        somma_dadi += dice_roll
-            stringa += "Totale: "+str(somma_dadi)
-            if (somma_costanti > 0):
-                stringa += " + "+str(somma_costanti)+" -> "+str(somma_dadi + somma_costanti)
-            if speech: invia_voce(chat["id"],stringa)
-            else: invia_testo(chat["id"],stringa)
+            roll.roll(mittente_username, comando, chat, canali, speech, invia_testo, invia_voce)
 
 
 ##        elif comando.startswith("/sendto"):
